fe012c50d118a109832caefd1ba38b2c5756fc0d
Scott Bender <scott@scottbender.net>
2018-01-15 21:38:01 -0500

    fix: catch exceptions from concentrator

--- conversions/wind.js
+++ conversions/wind.js
@@ -7,16 +7,23 @@ module.exports = (app, plugin) => {
     optionKey: 'WIND',
     keys: ["environment.wind.angleApparent", "environment.wind.speedApparent"],
     callback: (angle, speed) => {
-      return [
-        {
-          pgn: 130306,
-          buffer: Concentrate()
-          .uint8(0xff)
-            .uint16((speed*100).toFixed(0))
-            .uint16((angle * 10000).toFixed(0))
-            .result()
-        }
-      ]
+      try {
+        return [
+          {
+            pgn: 130306,
+            buffer: Concentrate()
+              .uint8(0xff)
+              .uint16((speed*100).toFixed(0))
+              .uint16((angle * 10000).toFixed(0))
+              .uint8(0xfa)
+              .uint8(0xff)
+              .uint8(0xff)
+              .result()
+          }
+        ]
+      } catch ( err ) {
+        console.error(err)
+      }
     }
   }
 }
