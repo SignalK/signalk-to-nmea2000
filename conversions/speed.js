04e3447ba119216cea9e9952ca7710905fd074bf
jtroyer76 <jtroyer@gmail.com>
2019-05-29 05:45:56 -0700

    feature: add Speed Through Water PGN (#25)

---
+++ conversions/speed.js
@@ -0,0 +1,22 @@
+
+module.exports = (app, plugin) => {
+  return {
+    pgns: [ 128259 ],
+    title: 'Speed (128259)',
+    optionKey: 'SPEED',
+    keys: ["navigation.speedThroughWater"],
+
+    callback: (speed) => {
+      try {
+        return [
+          {
+            pgn: 128259,
+            "Speed Water Referenced": speed,
+          }
+        ]
+      } catch ( err ) {
+        console.error(err)
+      }
+    }
+  }
+}
