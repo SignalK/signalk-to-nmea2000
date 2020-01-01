2f84a987cf7ed19d709172ea34ee82239a527938
Hans Tool <hans@defect.org>
2020-01-01 18:46:09 +0100

    Added Atmospheric Pressure using Environmental Parameters PGN 130311.

---
+++ conversions/environmentParameters.js
@@ -0,0 +1,22 @@
+
+module.exports = (app, plugin) => {
+  return {
+    pgns: [ 130311 ],
+    title: 'Evironmental Parameters (130311)',
+    optionKey: 'ENVIRONMENT_PARAMETERS',
+    keys: ["environment.outside.pressure"],
+
+    callback: (pressure) => {
+      try {
+        return [
+          {
+            pgn: 130311,
+            "Atmospheric Pressure": pressure,
+          }
+        ]
+      } catch ( err ) {
+        console.error(err)
+      }
+    }
+  }
+}
