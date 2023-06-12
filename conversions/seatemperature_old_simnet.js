fecc722b91e6fcdff8b636333949c2db9898ea00
valki2 <valki@valki.com>
2023-06-12 21:42:02 +0200

    Update seatemperature_old_simnet.js

--- conversions/seatemperature_old_simnet.js
+++ conversions/seatemperature_old_simnet.js
@@ -1,15 +1,15 @@
 
 module.exports = (app, plugin) => {
   return {
-    title: 'Sea/Air Temp (130311)',
-    optionKey: 'ENVIRONMENT_PARAMETERS_SEA',
+    title: 'Sea/Air Temp OLD Simnet (130311)',
+    optionKey: 'ENVIRONMENT_PARAMETERS_SEA_OLD',
     keys: ["environment.water.temperature"],
     callback: (water) => {
       try {
         return [
           {
 		  pgn: 130311,
-		  SID: 0xff,
+		  //SID: 0xff,
 		  "Water Temperature": water
           }
         ]
