9c9bac1ceac7f8fcb173d1a7ff44af5af96e0c88
andreas <andreas@wellenvogel.de>
2023-09-05 18:16:34 +0200

    allow to send 129284 also with greatCircle routing data

--- conversions/navigationdata.js
+++ conversions/navigationdata.js
@@ -13,7 +13,8 @@ module.exports = (app, plugin) => {
       "XTE mode": "Autonomous",
       "Navigation Terminated": "No"
     }]
-  }, {
+  }, 
+  {
     pgn: 129284,
     title: 'Navigation Data (129284)',
     optionKey: 'navigationdata',
@@ -58,5 +59,52 @@ module.exports = (app, plugin) => {
         "Waypoint Closing Velocity" : WCV,
       }]
     }
-  }]
+  },
+  {
+    pgn: 129284,
+    title: 'Navigation Data Great Circle (129284)',
+    optionKey: 'navigationdatagc',
+    keys: [
+      'navigation.courseGreatCircle.nextPoint.distance',
+      'navigation.courseGreatCircle.bearingToDestinationTrue',
+      'navigation.courseGreatCircle.bearingOriginToDestinationTrue',
+      'navigation.courseGreatCircle.nextPoint',
+      'navigation.courseGreatCircle.nextPoint.velocityMadeGood',
+      'notifications.arrivalCircleEntered',
+      'notifications.perpendicularPassed',
+      'navigation.courseGreatCircle.nextPoint.ID'
+    ],
+    timeouts: [
+      10000, 10000, 10000, 10000, 10000, undefined, undefined, 10000
+    ],
+    callback: (distToDest, bearingToDest, bearingOriginToDest, dest, WCV, ace, pp, wpid) => {
+      var dateObj = new Date();
+      var secondsToGo = Math.trunc(distToDest / WCV);
+      var etaDate = Math.trunc((dateObj.getTime() / 1000 + secondsToGo) / 86400);
+      var etaTime = (dateObj.getUTCHours() * (60 * 60) +
+                     dateObj.getUTCMinutes() * 60 +
+                     dateObj.getUTCSeconds() +
+                     secondsToGo) % 86400;
+
+      return [{
+        pgn: 129284,
+        "SID" : 0x88,
+        "Distance to Waypoint" :  distToDest,
+        "Course/Bearing reference" : 0,
+        "Perpendicular Crossed" : pp != null,
+        "Arrival Circle Entered" : ace != null,
+        "Calculation Type" : 0,
+        "ETA Time" : (WCV > 0) ? etaTime : undefined,
+        "ETA Date": (WCV > 0) ? etaDate : undefined,
+        "Bearing, Origin to Destination Waypoint" : bearingOriginToDest,
+        "Bearing, Position to Destination Waypoint" : bearingToDest,
+        "Origin Waypoint Number" : undefined,
+        "Destination Waypoint Number" : parseInt(wpid),
+        "Destination Latitude" : dest.latitude,
+        "Destination Longitude" : dest.longitude,
+        "Waypoint Closing Velocity" : WCV,
+      }]
+    }
+  }
+]
 }
