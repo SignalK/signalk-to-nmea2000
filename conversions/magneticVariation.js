fce13bb211de2f207298b23cb87f9befff6a6d76
alnavasa <alnavasa@hotmail.com>
2025-02-15 18:53:29 +0100

    Update errors on magnetic variation, testing process

--- conversions/magneticVariation.js
+++ conversions/magneticVariation.js
@@ -1,18 +1,32 @@
 module.exports = (app, plugin) => {
-    return {
-      title: 'Magnetic Variation',
-      optionKey: 'MAGNETIC_VARIATION',
-      keys: ["navigation.magneticVariation"],
-  
-      callback: (variation) => {
-        try {
-          return {
-            variation: variation, // Signal K ya usa radianes, no se convierte
-            source: 1 // 1 = Fuente fija de variación magnética
-          };
-        } catch (err) {
-          console.error("Error en magneticVariation.js:", err);
-        }
+  return {
+    pgns: [127258],  // Se añade la propiedad pgns como en leeway.js
+    title: 'Magnetic Variation (127258)',
+    optionKey: 'MAGNETIC_VARIATION',
+    keys: ["navigation.magneticVariation"],
+
+    callback: (variation) => {
+      try {
+        return [
+          {
+            pgn: 127258,
+            "Magnetic Variation": variation
+          }
+        ];
+      } catch (err) {
+        console.error("Error en magneticVariation.js:", err);
       }
-    };
+    },
+    tests: [{
+      input: [0.12], // Prueba con valor en radianes
+      expected: [{
+        "prio": 2,
+        "pgn": 127258,
+        "dst": 255,
+        "fields": {
+          "Magnetic Variation": 0.12
+        }
+      }]
+    }]
   };
+};
