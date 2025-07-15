6b81b8663d5e486619e21b331c9bf0720c784c5b
Scott Bender <scott@scottbender.net>
2025-07-14 22:19:43 -0400

    feature: add missing temperature sources

--- src/conversions/temperature.ts
+++ src/conversions/temperature.ts
@@ -1,5 +1,5 @@
 import { ServerAPI, Plugin, Delta, Update, PathValue, hasValues} from '@signalk/server-api'
-import { PGN } from '@canboat/ts-pgns'
+import { PGN, TemperatureSource } from '@canboat/ts-pgns'
 
 let tempMessage = (pgn:number, temp:number, inst:number, src:number) => {
   return {
@@ -70,70 +70,100 @@ function makeTemperature(pgn:number, prefix:string, info:any)
 
 const temperatures = [
   {
-    n2kSource: "Outside Temperature",
+    n2kSource: TemperatureSource.SeaTemperature,
+    source: 'environment.water.temperature',
+    instance: 101,
+    option: 'WATER'
+  },
+  {
+    n2kSource: TemperatureSource.OutsideTemperature,
     source: 'environment.outside.temperature',
     instance: 101,
     option: 'OUTSIDE'
   },
   {
-    n2kSource: "Inside Temperature",
+    n2kSource: TemperatureSource.InsideTemperature,
     source: 'environment.inside.temperature',
     instance: 102,
     option: 'INSIDE'
   },
   {
-    n2kSource: "Engine Room Temperature",
+    n2kSource: TemperatureSource.EngineRoomTemperature,
     source: 'environment.inside.engineRoom.temperature',
     instance: 103,
     option: 'ENGINEROOM'
   },
   {
-    n2kSource: "Main Cabin Temperature",
+    n2kSource: TemperatureSource.MainCabinTemperature,
     source: 'environment.inside.mainCabin.temperature',
     instance: 107 ,
     option: 'MAINCABIN'
   },
   {
-    n2kSource: "Refrigeration Temperature",
+    n2kSource: TemperatureSource.LiveWellTemperature,
+    source: 'environment.liveWell.temperature',
+    instance: 107 ,
+    option: 'LIVEWELL'
+  },
+  {
+    n2kSource: TemperatureSource.BaitWellTemperature,
+    source: 'environment.baitWell.temperature',
+    instance: 107 ,
+    option: 'BAITWELL'
+  },
+  {
+    n2kSource: TemperatureSource.RefrigerationTemperature,
     source: 'environment.inside.refrigerator.temperature',
     instance: 107 ,
     option: 'refridgerator'
   },
   {
-    n2kSource: "Heating System Temperature",
+    n2kSource: TemperatureSource.HeatingSystemTemperature,
     source: 'environment.inside.heating.temperature',
     instance: 107,
     option: 'HEATINGSYSTEM'
   },
   {
-    n2kSource: "Dew Point Temperature",
+    n2kSource: TemperatureSource.DewPointTemperature,
     source: 'environment.outside.dewPointTemperature',
     instance:107 ,
     option: 'DEWPOINT'
   },
   {
-    n2kSource: "Apparent Wind Chill Temperature",
+    n2kSource: TemperatureSource.ApparentWindChillTemperature,
     source: 'environment.outside.apparentWindChillTemperature',
     instance: 107,
     option: 'APPARENTWINDCHILL'
   },
   {
-    n2kSource: "Theoretical Wind Chill Temperature",
+    n2kSource: TemperatureSource.TheoreticalWindChillTemperature,
     source: 'environment.outside.theoreticalWindChillTemperature',
     instance: 107 ,
     option: 'THEORETICALWINDCHILL'
   },
   {
-    n2kSource: "Heat Index Temperature",
+    n2kSource: TemperatureSource.HeatIndexTemperature,
     source: 'environment.outside.heatIndexTemperature',
     instance: 107 ,
     option: 'HEATINDEX'
   },
   {
-    n2kSource: "Freezer Temperature",
+    n2kSource: TemperatureSource.FreezerTemperature,
     source: 'environment.inside.freezer.temperature',
     instance: 107 ,
     option: 'FREEZER'
+  },
+  {
+    n2kSource: TemperatureSource.ExhaustGasTemperature,
+    source: 'environment.exhaustGas.temperature',
+    instance: 107 ,
+    option: 'EXHAUSTGAS'
+  },
+  {
+    n2kSource: TemperatureSource.ShaftSealTemperature,
+    source: 'environment.shaftSeal.temperature',
+    instance: 107 ,
+    option: 'SHAFTSEAL'
   }
 ]
 
